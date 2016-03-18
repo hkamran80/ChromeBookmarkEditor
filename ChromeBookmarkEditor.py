2f90bb2df19d2c24e471a3ea5856b04a48be98a6
robperc <robperc@uw.edu>
2016-03-18 12:39:08 -0700

    Added write method to ChromeBookmarks class

--- ChromeBookmarkEditor.py
+++ ChromeBookmarkEditor.py
@@ -10,6 +10,7 @@ class ChromeBookmarks(object):
 	def __init__(self):
 		self.epoch    = "13078095537020784" # Haven't reverse engineered the epoch date_added is calculated from so this is hard coded for now
 		self.path     = self.get()
+		self.json     = None
 		self.ids      = None
 		self.children = None
 		self.read()
@@ -27,7 +28,8 @@ class ChromeBookmarks(object):
 	def read(self):
 		with open(self.path, "r") as infile:
 			js = json.load(infile)
-		self.children = js['roots']['bookmark_bar']['children']
+		self.json = js
+		self.children = self.json['roots']['bookmark_bar']['children']
 		self.ids = [bm['id'] for bm in self.children]
 
 	def add(self, title, url):
@@ -45,6 +47,12 @@ class ChromeBookmarks(object):
 			if child['name'] == title:
 				self.children.remove(child)
 
+	def write(self):
+		bak_file = self.path + ".bak"
+		os.remove(bak_file)
+		self.json.pop("checksum", None)
+		with open(self.path, "w") as outfile:
+			json.dump(self.json, outfile)
 
 
 
